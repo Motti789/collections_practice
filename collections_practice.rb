0f666cdce23f4a89343c28a108873813c4a960f4
Motti789 <mchillywilly@gmail.com>
2021-07-06 14:53:13 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
+def sort_array_asc(i)
+    i.sort
+end
 
+def sort_array_desc(i)
+    i.sort.reverse
+end    
+
+def sort_array_char_count(strings)
+    strings.sort_by  {|x| x.length}
+end    
+
+def swap_elements(array)
+   array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(i)
+    i.reverse
+end    

2bf86172d6cd06458836587a30b740583bac4566
Motti789 <mchillywilly@gmail.com>
2021-07-06 15:31:00 -0400

    commit tree 64718ba76cc357f18b8d8a45a203d98a46eedbf7

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,3 +17,27 @@ end
 def reverse_array(i)
     i.reverse
 end    
+
+def kesha_maker(array)
+    array.each do |item|
+      item[2] = "$"
+    end
+  end
+
+def find_a(array)
+    array.select {|m| m.start_with?("a")}
+end
+
+def sum_array(array)
+    array.sum
+end    
+
+def add_s(array)
+    array.collect do |word|
+      if array[1] == word
+        word
+      else
+        word + "s"
+      end
+    end
+  end
